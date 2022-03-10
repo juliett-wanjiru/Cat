08970cf79a51de26c038517cb4c646cadd8993c7
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-03-10 08:37:59 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,42 @@
+fun main(){
+name()
+    modulus()
+    sentence()
+    school()
+
+
+
+
+}
+fun name(){
+    var name:String="codeHIVE"
+    var name1= "h"
+    var name2 ="i"
+    var name3 ="v"
+    var name4 ="e"
+    println(name1+name2+name3+name4)
+
+
+
+
+
+}
+fun modulus(){
+    var a= 7
+    var b= 2
+    var modulus= 7%2
+    println(modulus)
+}
+fun sentence(){
+    var name= "juliet"
+    var age= 19
+    var sentence= "hi my name is$  and i am $age"
+    println(sentence)
+}
+fun school(){
+    var school= "precious"
+    println(school)
+
+    }
+
+
